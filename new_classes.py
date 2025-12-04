11587fa8d4f8e5c349f33237e21485b34b9227e4
Paulo <gomespc798@gmail.com>
2025-12-04 15:23:02 -0300

    se lascamo kkkkkkk

--- new_classes.py
+++ new_classes.py
@@ -2,13 +2,13 @@ class membro_universidade:
     def __init__(self, nome, email, matricula):
         self.nome = nome
         self.email = email
-        self.matricula = matricula
+        self.__matricula = matricula
 
     def exibir_dados(self):
         print("\nDados do membro")
         print("Nome:", self.nome)
         print("Email:", self.email)
-        print("Matricula:", self.matricula)
+        print("Matricula:", self.__matricula)
 
 
 class disciplina:
@@ -56,3 +56,27 @@ class professor(membro_universidade):
             brl = str(self.salario)
         print("Salario: R$ " + brl)
         # Teste commit branch
+
+membro_generico = membro_universidade("João Silva", "joao.silva@uni.br", "12345")
+
+disciplina_poo = disciplina("SI001", "Programação Orientada a Objetos", "2024.1", "60h", "POO")
+
+
+aluno_exemplo = aluno("Maria Souza", "maria.souza@aluno.br", "2024001", 8.5)
+
+professor_exemplo = professor(
+    "Dr. Carlos Lima",
+    "carlos.lima@prof.br",
+    "P9876",
+    "Ciência da Computação",
+    "Doutorado",
+    8500.75
+)
+
+
+print("--- Testando as Instâncias ---")
+membro_generico.exibir_dados()
+disciplina_poo.exibir_dados()
+aluno_exemplo.exibir_dados()
+professor_exemplo.exibir_dados()
+print("------------------------------")
